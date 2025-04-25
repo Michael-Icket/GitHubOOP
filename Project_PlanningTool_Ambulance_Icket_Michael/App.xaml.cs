fe218005797a08cbbf35d9d480110f216b48bc6b
michaelicket <michael.icket@student.vives.be>
2025-04-25 10:13:17 +0200

    Start-up

---
+++ Project_PlanningTool_Ambulance_Icket_Michael/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Project_PlanningTool_Ambulance_Icket_Michael
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
